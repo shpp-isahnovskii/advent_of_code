16872aa96c49d5fb08cab3ea8a26ebede4bae2aa
noty-ivan <ivan.sakhnovskyi@noty.ai>
2023-12-03 23:57:30 +0200

    names refactoring

--- 2023/day_01/index.js
+++ 2023/day_01/index.js
@@ -4,16 +4,16 @@ const filePath = './codes.txt';
 const dataArr = fs.readFileSync(filePath, 'utf8').split('\n');
 
 const getCode = (arr) => {
-  return arr.reduce((acc, el) => getCalibration(el) + acc, 0);
+  return arr.reduce((acc, el) => getDigit(el) + acc, 0);
 };
 
-const getCalibration = (codeString) => {
-  const digitChar = /\d/g;
-  const arrayOfCodes = codeString.match(digitChar);
-  return decryptAnArray(arrayOfCodes);
+const getDigit = (codeString) => {
+  const digitRegex = /\d/g;
+  const arrayOfDigits = codeString.match(digitRegex);
+  return getNumber(arrayOfDigits);
 };
 
-const decryptAnArray = (array) => {
+const getNumber = (array) => {
   return +(array.length > 1 ? `${array[0]}${array[array.length - 1]}`: `${array[0]}${array[0]}`);
 };
 
